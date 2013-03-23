040ee5faac7b07a61a52dccb3dc8822f7b3bf566
cannibal <cannibal234@gmail.com>
2013-03-23 21:32:01 +0000

    - Adds add file method

--- src/Versionable/Prospect/File/CollectionInterface.php
+++ src/Versionable/Prospect/File/CollectionInterface.php
@@ -2,9 +2,13 @@
 
 namespace Versionable\Prospect\File;
 
+use Versionable\Prospect\File\FileInterface;
+
 interface CollectionInterface
 {
   public function toString();
+
+  public function add(FileInterface $file);
 
   public function setBoundary($boundary);
 }
